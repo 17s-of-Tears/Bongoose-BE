8a682aa98dad733f67ffc129306f4f3aae704cc5
codingjoa <taek55@gmail.com>
2021-10-04 03:07:36 +0900

    211004

--- loadModules.js
+++ loadModules.js
@@ -1,11 +1,4 @@
 const path = require('path');
-const HOME = process.env.HOME;
-const maria = require(path.join(HOME, 'pman/server/database'));
-const app = require(path.join(HOME, 'pman/server/server'));
-const env = require(path.join(HOME, 'pman/server/environment'));
-const jwt = require(path.join(HOME, 'pman/server/jwt'));
-const Error = require(path.join(HOME, 'pman/server/Types/Error'));
-
-module.exports = {
-  maria, app, env, jwt, Error
-};
+const JEKO_HOME = process.env.JEKO_HOME;
+//module.exports = require(path.join(JEKO_HOME, 'index'));
+module.exports = require(JEKO_HOME);
